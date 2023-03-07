ce0e625d257b23b3b83129c2d5b222c99935667f
Marcos_ <marcosde@buffalo.edu>
2023-03-06 19:02:45 -0500

    GUI build out with other classes for data handling

---
+++ LaunchDrum.py
@@ -0,0 +1,33 @@
+#--------------------------#
+# CSE 453: Rocket Launcher Team
+# Author: Marcos
+# Date: 03/6/2023
+# Launch drum class provides simple way for the serial comunication to package 
+# data about the state of each launch drum to the MasterGUI
+#--------------------------#
+
+# TODO replace the json string with imported serial communication 
+json = ""
+
+class LaunchDrum():
+    def __init__(self,json):
+        # TODO find a way to populate this object with relative information from the JSON
+        pass
+
+    # returns the angle of elevation of the launch drum
+    def getHeight(self):
+        return "30%"
+    
+    # returns the rotation of the launch drum
+    def getRotation(self):
+        return "180"
+    
+    # returns true when the corresponding green switch has been activated
+    def getActivationStatus(self):
+        return False
+    
+    # returns the pressurization status for the launch drum
+    def getPressurizationStatus(self):
+        return False
+    
+    # Eventually we will use this Class to give highlevel commands 
--- MasterGUI.py
+++ MasterGUI.py
@@ -3,6 +3,7 @@ import tkinter.messagebox
 import customtkinter
 import os
 from PIL import Image
+from LaunchDrum import *
 
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -10,11 +11,15 @@ customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "gre
 
 
 class App(customtkinter.CTk):
-    def __init__(self):
+    def __init__(self, rightLaunchDrum, leftLaunchDrum):
         super().__init__()
 
+        #Launch Drum controllers
+        self.rDrum = rightLaunchDrum
+        self.lDrum = leftLaunchDrum
+
         # configure window
-        self.title("University at Buffalo Rocket Launcher")
+        self.title("University at Buffalo Rocket Launcher Software")
         self.geometry(f"{1100}x{580}")
 
 
@@ -23,6 +28,7 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
+         #----------Left Frame----------#
         # create sidebar frame with widgets
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
@@ -35,7 +41,6 @@ class App(customtkinter.CTk):
         # Program title
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Rocket Launcher", image = self.Rocket_logo, font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=0, pady=(20, 0))
-
  
         # create radiobutton frame
         self.state_var = tkinter.IntVar(value=0)
@@ -51,12 +56,8 @@ class App(customtkinter.CTk):
         self.launch_state_button.grid(row=5, column=0, pady=10, padx=20, sticky="n")
 
 
-        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
-        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
-                                                               command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
+        #----------Middle Frame----------#
         # create main entry and button
         self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
         self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
@@ -81,47 +82,47 @@ class App(customtkinter.CTk):
         self.launch_progressbar.grid(row=0, column=0, padx=(20, 20), pady=(10, 10), sticky="ew")
 
         #Load
-        self.Load_textbox = customtkinter.CTkTextbox(self.tabview.tab("Load Stage"), width=250)
+        self.Load_textbox = customtkinter.CTkTextbox(self.tabview.tab("Load Stage"), width=250, height=350)
         self.Load_textbox.grid(row=1,rowspan=2, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
         
 
         #Aim
-        self.Aim_textbox = customtkinter.CTkTextbox(self.tabview.tab("Aim Stage"), width=250)
+        self.Aim_textbox = customtkinter.CTkTextbox(self.tabview.tab("Aim Stage"), width=250, height=350)
         self.Aim_textbox.grid(row=0,rowspan=2, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
         #Pressurization
-        self.Pressurization_textbox = customtkinter.CTkTextbox(self.tabview.tab("Pressurization"), width=250)
+        self.Pressurization_textbox = customtkinter.CTkTextbox(self.tabview.tab("Pressurization"), width=250, height=350)
         self.Pressurization_textbox.grid(row=0,rowspan=2, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
         #Launch
-        self.Launch_textbox = customtkinter.CTkTextbox(self.tabview.tab("Launch Stage"), width=250)
+        self.Launch_textbox = customtkinter.CTkTextbox(self.tabview.tab("Launch Stage"), width=250, height=350)
         self.Launch_textbox.grid(row=0,rowspan=2, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
+
+        #----------Right Frame----------#
         # Left Rocket information
         self.Left_Rocket_frame = customtkinter.CTkFrame(self)
         self.Left_Rocket_frame.grid(row=0, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
-        self.Left_Rocket_label_group = customtkinter.CTkLabel(master=self.Left_Rocket_frame, text="Left Rocket information")
-        self.Left_Rocket_label_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
+        self.Left_Rocket_label_group = customtkinter.CTkLabel(master=self.Left_Rocket_frame, text="Left Rocket Information:")
+        self.Left_Rocket_label_group.grid(row=0, column=0, columnspan=1, padx=5, pady=5, sticky="")
+        self.Left_Rocket_textbox = customtkinter.CTkTextbox(master=self.Left_Rocket_frame, width=200, height=150)
+        self.Left_Rocket_textbox.grid(row=1, column=0, padx=(20, 20), pady=(10, 10), sticky="nsew")
+
 
         # Right Rocket information
         self.Right_Rocket_frame = customtkinter.CTkFrame(self)
         self.Right_Rocket_frame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
-        self.Right_Rocket_label_group = customtkinter.CTkLabel(master=self.Right_Rocket_frame, text="Right Rocket information")
-        self.Right_Rocket_label_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
+        self.Right_Rocket_label_group = customtkinter.CTkLabel(master=self.Right_Rocket_frame, text="Right Rocket Information:")
+        self.Right_Rocket_label_group.grid(row=0, column=0, columnspan=1, padx=5, pady=5, sticky="")
+        self.Right_Rocket_textbox = customtkinter.CTkTextbox(master=self.Right_Rocket_frame, width=200, height=150)
+        self.Right_Rocket_textbox.grid(row=1, column=0, padx=(20, 20), pady=(10, 10), sticky="nsew")
 
-
-        # set default values
-        self.scaling_optionemenu.set("100%")
+        # set stage text values
         self.Load_textbox.insert("0.0", "CTkTextbox\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 20)
 
-    def open_input_dialog_event(self):
-        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
-        print("CTkInputDialog:", dialog.get_input())
 
-    def change_scaling_event(self, new_scaling: str):
-        new_scaling_float = int(new_scaling.replace("%", "")) / 100
-        customtkinter.set_widget_scaling(new_scaling_float)
 
+    # updates the GUI when a state change occurs
     def change_state(self, state):
         if (state >= 0 and state < 4):
             self.state_var = state
@@ -147,10 +148,24 @@ class App(customtkinter.CTk):
                 self.launch_state_button.configure(state="enabled")
 
 
+    def updateLaunchDrumInformation(self):
+        rHeight = rDrum.getHeight()
+        lHeight = lDrum.getHeight()
+        rRotate = rDrum.getRotation()
+        lRotate = lDrum.getRotation()
+        rActive = rDrum.getActivationStatus()
+        lActive = lDrum.getActivationStatus()
+        self.Left_Rocket_textbox.insert("0.0","Activation status: "+str(lActive)+"\nHeight:"+str(lHeight)+"\nRotation"+str(lRotate))
+        self.Right_Rocket_textbox.insert("0.0","Activation status: "+str(rActive)+"\nHeight:"+str(rHeight)+"\nRotation"+str(rRotate))
+
+
 
 
         
 
 if __name__ == "__main__":
-    app = App()
+    rDrum = LaunchDrum("hello")
+    lDrum = LaunchDrum("hello")
+    app = App(rDrum,lDrum)
+    app.updateLaunchDrumInformation()
     app.mainloop()
